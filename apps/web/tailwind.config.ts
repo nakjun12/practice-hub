332da52929374838ec745522b5e893ee387cf2fd
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-02-12 13:44:53 +0900

    feat : tailwindCSS config 추가 및 적용

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind config is required for editor support
+
+import sharedConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss";
+
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [sharedConfig]
+};
+
+export default config;
