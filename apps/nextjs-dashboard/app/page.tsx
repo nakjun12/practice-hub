ee18ca992096329c064e41529762c9f2fd4960b7
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-02-14 09:26:37 +0900

    feat: next chapter 3~5

--- apps/nextjs-dashboard/app/page.tsx
+++ apps/nextjs-dashboard/app/page.tsx
@@ -1,5 +1,5 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
@@ -28,6 +28,20 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+      />
     </main>
   );
 }
